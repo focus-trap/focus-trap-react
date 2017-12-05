fd1bb769071fd960902cdb5293b0995ee0f63a12
Ted Driggs <ted.driggs@outlook.com>
2017-12-05 14:39:49 -0800

    Support pass-through to underlying DOM element
    Add proper typing for common HTML properties, and allow other unknown properties to be passed through without causing typecheck failures.

--- index.d.ts
+++ index.d.ts
@@ -2,11 +2,15 @@ import { Options as FocusTrapOptions } from "focus-trap";
 import React from "react";
 
 declare module "focus-trap-react" {
-  export interface Props {
+  // Extend AllHTMLAttributes to provide completions on DOM properties wherever 
+  // React does.
+  export interface Props extends React.AllHTMLAttributes<any> {
     active?: boolean;
     paused?: boolean;
     tag?: string;
     focusTrapOptions?: FocusTrapOptions;
+    // Allow through any properties that weren't picked up
+    [x: string]: any;
   }
 
   export default class FocusTrap extends React.Component<Props> {}
