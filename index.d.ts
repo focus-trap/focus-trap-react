5900c609643fccbb5f77828318cf8b71ccee43df
Ted Driggs <ted.driggs@outlook.com>
2017-12-17 12:40:42 -0800

    Add TypeScript declarations (#21)
    * Add TypeScript declarations
    
    * Support pass-through to underlying DOM element
    
    Add proper typing for common HTML properties, and allow other unknown properties to be passed through without causing typecheck failures.
    
    * Move imports inside declaration

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+declare module "focus-trap-react" {
+  import { Options as FocusTrapOptions } from "focus-trap";
+  import React from "react";
+
+  // Extend AllHTMLAttributes to provide completions on DOM properties wherever
+  // React does.
+  export interface Props extends React.AllHTMLAttributes<any> {
+    active?: boolean;
+    paused?: boolean;
+    tag?: string;
+    focusTrapOptions?: FocusTrapOptions;
+    // Allow through any properties that weren't picked up
+    [x: string]: any;
+  }
+
+  export default class FocusTrap extends React.Component<Props> {}
+}
