227c6e161ddb05d05faeda3998d82c5f2e8d5a3f
Benjamin Koltes <ayc0.benj@gmail.com>
2020-06-20 19:26:50 -0400

    Update index.d.ts (#66)
    children's type is `React.ReactNode` and not `React.ReactElement`

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ export = FocusTrap;
 
 declare namespace FocusTrap {
   export interface Props extends React.AllHTMLAttributes<any> {
-    children: React.ReactElement<any>;
+    children: React.ReactNode;
     active?: boolean;
     paused?: boolean;
     focusTrapOptions?: FocusTrapOptions;
