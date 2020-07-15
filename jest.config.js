03e0125063700d16fed1b366fa5f865cdc24a46b
Marais Rossouw <me@maraisr.com>
2020-07-15 18:09:35 +1000

    Introduces changesets for release management (#79)

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.[t|j]sx?$': 'babel-jest'
   },
-  setupFilesAfterEnv: [
-    '<rootDir>/test/jest-setup.js'
-  ],
+  setupFilesAfterEnv: ['<rootDir>/test/jest-setup.js'],
   clearMocks: true,
   testURL: 'http://localhost'
-}
+};
