3e329e560d99beb0c2ceedcc3f52c515a3b4d66f
Tyler Hawkins <thawkin3@byu.net>
2020-12-18 22:47:58 -0700

    refactor: undo spacing changes in demo file

--- demo/js/demo-containerelements-childless.js
+++ demo/js/demo-containerelements-childless.js
@@ -55,7 +55,7 @@ class DemoContainerElementsChildless extends React.Component {
 
         <div className="trap is-active">
           <p ref={this.setElementRef('element1')}>
-            Here is a focus trap <a href="#">with</a> <a href="#">some</a>{' '}
+            Here is a focus trap <a href="#">with</a> <a href="#">some</a>
             <a href="#">focusable</a> parts.
           </p>
           <p>
@@ -63,7 +63,8 @@ class DemoContainerElementsChildless extends React.Component {
           </p>
           <p ref={this.setElementRef('element2')}>
             Here is a another focus trap element. <a href="#">See</a>{' '}
-            <a href="#">how</a> it <a href="#">works</a>.
+            <a href="#">how</a>
+            it <a href="#">works</a>.
           </p>
           <p>
             <button
