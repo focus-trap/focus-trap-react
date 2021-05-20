af1e59cc7bce27d6294a30820ce987c039fa9706
Daniel Tonon <dan503.npm@gmail.com>
2021-05-20 20:28:41 +1000

    Restoring default demo back to the version that is on master

--- demo/js/demo-defaults.js
+++ demo/js/demo-defaults.js
@@ -1,47 +1,44 @@
-const { useState } = require('react');
 const React = require('react');
 const ReactDOM = require('react-dom');
 const FocusTrap = require('../../dist/focus-trap-react');
 
 const container = document.getElementById('demo-defaults');
 
-const DemoDefaults = () => {
-  const [isTrapActive, setIsTrapActive] = useState(false);
+class DemoDefaults extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <>
-      <style>{`
-      .trap {
-        position: absolute;
-        opacity: 0;
-        visibility: hidden;
-        transition: opacity 0.5s, visibility 0.5s;
-      }
-      .trap.is-active {
-        opacity: 1;
-        visibility: visible;
-      }
-      `}</style>
-      <div>
-        <p>
-          <button
-            onClick={() => setIsTrapActive(true)}
-            aria-label="activate trap for 'defaults' demo"
-          >
-            activate trap
-          </button>
-        </p>
-      </div>
+    this.state = {
+      activeTrap: false,
+    };
 
-      <FocusTrap active={isTrapActive} activationDelay={10}>
-        <div className={['trap', isTrapActive ? 'is-active' : ''].join(' ')}>
+    this.mountTrap = this.mountTrap.bind(this);
+    this.unmountTrap = this.unmountTrap.bind(this);
+  }
+
+  mountTrap() {
+    this.setState({ activeTrap: true });
+  }
+
+  unmountTrap() {
+    this.setState({ activeTrap: false });
+  }
+
+  render() {
+    const trap = this.state.activeTrap && (
+      <FocusTrap
+        focusTrapOptions={{
+          onDeactivate: this.unmountTrap,
+        }}
+      >
+        <div className="trap is-active">
           <p>
             Here is a focus trap <a href="#">with</a> <a href="#">some</a>{' '}
             <a href="#">focusable</a> parts.
           </p>
           <p>
             <button
-              onClick={() => setIsTrapActive(false)}
+              onClick={this.unmountTrap}
               aria-label="deactivate trap for 'defaults' demo"
             >
               deactivate trap
@@ -49,8 +46,22 @@ const DemoDefaults = () => {
           </p>
         </div>
       </FocusTrap>
-    </>
-  );
-};
+    );
+
+    return (
+      <div>
+        <p>
+          <button
+            onClick={this.mountTrap}
+            aria-label="activate trap for 'defaults' demo"
+          >
+            activate trap
+          </button>
+        </p>
+        {trap}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<DemoDefaults />, container);
